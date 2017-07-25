8d92fb662fed7c89223b0f775a56cfd60cb40fa1
Paulo Henrique Martins <paulo.martins@savedo.de>
2017-07-25 17:18:28 +0200

    adding admin protection

--- app/admin.php
+++ app/admin.php
@@ -1,7 +1,20 @@
 <?php
   include("inc/global.php");
-  include($magrathea_path."/MagratheaAdmin.php"); // $magrathea_path should already be declared
- 
-  $admin = new MagratheaAdmin(); // adds the admin file
-  $admin->Load(); // load!
+  include($magrathea_path."/MagratheaAdmin.php"); //  should already be declared
+
+  class MagratheaLoginController extends MagratheaController {
+    public static function Login(){
+      if(@$_GET["error"] == "login_error")
+        self::GetSmarty()->assign("message", "Login or password incorrect!");
+      self::GetSmarty()->display("login.html");
+    }
+  }
+
+  if(!empty($_SESSION["magrathea_user"])) {
+    $admin = new MagratheaAdmin(); // adds the admin file
+    $admin->Load(); // load!
+  } else {
+    MagratheaLoginController::Login();
+  }
+
 ?>
