059750c90dc4a76e4fa654305465a17881549c7e
platypusweb <platypusweb@john-morton.dreamhost.com>
2017-08-03 02:20:08 -0700

    cycle object

---
+++ app/Models/Cycle.php
@@ -0,0 +1,13 @@
+<?php
+
+include(__DIR__."/Base/CycleBase.php");
+
+class Cycle extends CycleBase {
+	// your code goes here!
+}
+
+class CycleControl extends CycleControlBase {
+	// and here!
+}
+
+?>
---
+++ app/Models/FollowFarm.php
@@ -0,0 +1,13 @@
+<?php
+
+include(__DIR__."/Base/FollowFarmBase.php");
+
+class FollowFarm extends FollowFarmBase {
+	// your code goes here!
+}
+
+class FollowFarmControl extends FollowFarmControlBase {
+	// and here!
+}
+
+?>
